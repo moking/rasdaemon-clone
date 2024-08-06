31ff87569086aad03265a17f118f1d3069eeec60
Fan Ni <nifan.cxl@gmail.com>
2024-08-06 11:37:37 -0700

    run-me.sh: add script to compile rasdaemon

---
+++ run-me.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+#
+install_preps() {
+    found=`sudo find /etc/ -name "sources.list" | grep -c "list"`
+    if [ "found" == "0" ];then
+        echo "Currently only distro based on debian release supported"
+        exit 1
+    fi
+    sudo apt-get install  make gcc autoconf automake libtool \
+        libtraceevent-dev tar 
+}
+
+configure() {
+    autoreconf -vfi 
+    ./configure --enable-aer --enable-mce
+}
+
+compile() {
+    make
+}
+
+install_preps
+configure
+compile
